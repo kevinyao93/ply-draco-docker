100cf9ea8398295e9d3ff2a969083c755a155700
Kevin Yao <kevinyao93@gmail.com>
2022-04-26 09:35:39 -0700

    updated setup

--- data/create_subsets.sh
+++ data/create_subsets.sh
@@ -1,46 +1,13 @@
-'''
-#wget http://plenodb.jpeg.org/pc/8ilabs/longdress.zip
-unzip longdress.zip
-rm *.pdf
-rm *.docx
-cd longdress
-rm -R Png
-rm Counts.txt
-rm longdress_vox10.mp4
-cd ../
-rm *.pdf
-rm *.docx
-
-#wget http://plenodb.jpeg.org/pc/8ilabs/redandblack.zip
-unzip redandblack.zip
-cd redandblack
-rm -R Png
-rm Counts.txt
-rm redandblack_vox10.mp4
-cd ../
-rm *.pdf
-rm *.docx
-
-#wget http://plenodb.jpeg.org/pc/8ilabs/soldier.zip
-unzip soldier.zip
-cd soldier
-rm -R Png
-rm Counts.txt
-rm soldier_vox10.mp4
-cd ../
-rm *.pdf
-rm *.docx
-
-'''
 function create_subset() {
     echo $1
     mkdir $1_$2
     cd $1/Ply
     for eachfile in *.ply
     do 
-        open -a CloudCompare.app --args -SILENT -O /Users/kevinyao/Documents/Volumetric_Video/ply-draco-docker/data/$1/Ply/$eachfile -SS RANDOM $2 -C_EXPORT_FMT PLY -PLY_EXPORT_FMT ASCII -AUTO_SAVE ON -NO_TIMESTAMP -SAVE_CLOUDS -CLEAR ;
-        echo "finish" /Users/kevinyao/Documents/Volumetric_Video/ply-draco-docker/data/$1/Ply/$eachfile
-        sleep 2
+        #open -a CloudCompare.app --args -SILENT -O /Users/kevinyao/Documents/Volumetric_Video/ply-draco-docker/data/$1/Ply/$eachfile -SS RANDOM $2 -C_EXPORT_FMT PLY -PLY_EXPORT_FMT ASCII -AUTO_SAVE ON -NO_TIMESTAMP -SAVE_CLOUDS -CLEAR ;
+        cloudcompare.CloudCompare -SILENT -O /home/kevin/ply-draco-docker/data/$1/Ply/$eachfile -SS RANDOM $2 -C_EXPORT_FMT PLY -PLY_EXPORT_FMT ASCII -AUTO_SAVE ON -NO_TIMESTAMP -SAVE_CLOUDS -CLEAR
+        echo "finish" /$1/Ply/$eachfile
+        sleep 1
     done
     rm *.bin
     mv *SUBSAMPLED* ../../$1_$2
---
+++ data/setup.sh
@@ -0,0 +1,31 @@
+wget http://plenodb.jpeg.org/pc/8ilabs/longdress.zip
+unzip longdress.zip
+rm *.pdf
+rm *.docx
+cd longdress
+rm -R Png
+rm Counts.txt
+rm longdress_vox10.mp4
+cd ../
+rm *.pdf
+rm *.docx
+
+wget http://plenodb.jpeg.org/pc/8ilabs/redandblack.zip
+unzip redandblack.zip
+cd redandblack
+rm -R Png
+rm Counts.txt
+rm redandblack_vox10.mp4
+cd ../
+rm *.pdf
+rm *.docx
+
+wget http://plenodb.jpeg.org/pc/8ilabs/soldier.zip
+unzip soldier.zip
+cd soldier
+rm -R Png
+rm Counts.txt
+rm soldier_vox10.mp4
+cd ../
+rm *.pdf
+rm *.docx
